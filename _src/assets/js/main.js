73ffe86ecbfdb6a6730039694fe84d63d7a8060f
valerialp <valeria.lorentep@gmail.com>
2019-04-08 18:12:52 +0200

    complete js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-console.log('>> Ready :)');
 //recoger todos los elementos necesarios del html
 const countEl = document.querySelector('.count');
 const inputEl = document.querySelector('#number');
@@ -16,22 +15,27 @@ function getRandomNumber(max) {
 let randomNumber = getRandomNumber(100);
 console.log(randomNumber);
 
-//crear una funcion handler para el boton
-function testing(){
+//crear una funcion handler para el boton 
+function testing() {
     console.log(Number(inputEl.value));
-    if(Number(inputEl.value) === randomNumber){
-        trackEl.innerHTML = '¡HASGANADO, CAMPEONA!';
-    } else if(Number(inputEl.value) > randomNumber){
+    if(inputEl.value === ''){
+        trackEl.innerHTML = 'Pon un número';
+        return ;
+    } else if (Number(inputEl.value) === randomNumber) {
+        trackEl.innerHTML = '¡HAS GANADO, CAMPEONA!';
+    } else if (Number(inputEl.value) > randomNumber) {
         trackEl.innerHTML = 'Demasiado alto';
-    } else if(Number(inputEl.value) < randomNumber){
+    } else if (Number(inputEl.value) < randomNumber) {
         trackEl.innerHTML = 'Demasiado bajo';
     }
+    let countValue = Number(countEl.innerHTML);
+
+    //bucle para el contador
+    for (let i = 0; i < 1; i++) {
+        countValue = countValue + 1;
+        countEl.innerHTML = countValue;
+    }
 }
+
 //poner un listener al boton
 buttonEl.addEventListener('click', testing);
-
-
-
-  
-  // Anda, chacho, píntame en la consola un número random hasta 100;
-//   console.log('> ' + getRandomNumber(100));
