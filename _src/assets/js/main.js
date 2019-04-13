a5890f9da0a3a37e5c98f195c0b695a56e8a5a82
valerialp <valeria.lorentep@gmail.com>
2019-04-13 16:17:50 +0200

    fix js and add got style

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,42 +1,54 @@
 'use strict';
 
-//recoger todos los elementos necesarios del html
 const countEl = document.querySelector('.count');
 const inputEl = document.querySelector('#number');
-const buttonEl = document.querySelector('.btn');
+const buttonEl = document.querySelector('.btn-try');
+const buttonResetEl = document.querySelector('.btn-reset');
 const trackEl = document.querySelector('.track');
-
-//la función de random number se ejecuta al entran en la pag
-// Función que genera un número aleatorio hasta un máximo dado
-function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-}
-
 let randomNumber = getRandomNumber(100);
 console.log('Este es el número random -> ' + randomNumber);
 
-//crear una funcion handler para el boton 
-function testing() {
-    console.log(Number(inputEl.value));
-    if (inputEl.value === '') {
-        trackEl.innerHTML = 'Pon un número';
-        return;
-    } else if (Number(inputEl.value) === randomNumber) {
-        trackEl.innerHTML = '¡HAS GANADO, CAMPEONA!';
-        return;
-    } else if (Number(inputEl.value) > randomNumber) {
-        trackEl.innerHTML = 'Demasiado alto';
-    } else if (Number(inputEl.value) < randomNumber) {
-        trackEl.innerHTML = 'Demasiado bajo';
-    }
-    let countValue = Number(countEl.innerHTML);
-
-    //bucle para el contador
-    for (let i = 0; i < 1; i++) {
-        countValue = countValue + 1;
-        countEl.innerHTML = countValue;
-    }
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
 }
 
-//poner un listener al boton
-buttonEl.addEventListener('click', testing);
+// function printFeedback(){
+
+// }
+
+function feedback(){
+  const inputNumber = Number(inputEl.value);
+  if (inputEl.value === '') {
+    trackEl.innerHTML = 'Pon un número';
+    return;
+  } else if (inputNumber === randomNumber) {
+    trackEl.innerHTML = '¡HAS GANADO, CAMPEONA!';
+    return;
+  } else if (inputNumber > randomNumber) {
+    trackEl.innerHTML = 'Demasiado alto';
+  } else if (inputNumber < randomNumber) {
+    trackEl.innerHTML = 'Demasiado bajo';
+  }
+}
+
+function counterAttempts(){
+  let countValue = Number(countEl.innerHTML);
+  countValue += 1;
+  countEl.innerHTML = countValue;
+}
+
+function handlerTestingNumber() {
+  feedback();
+  counterAttempts();
+}
+
+function handlerResetAll(){
+  countEl.innerHTML = 0;
+  inputEl.value = '';
+  trackEl.innerHTML = 'Escribe un número y dale a prueba';
+  getRandomNumber(100);
+}
+
+buttonEl.addEventListener('click', handlerTestingNumber);
+buttonResetEl.addEventListener('click', handlerResetAll);
+inputEl.addEventListener('', handlerTestingNumber);
